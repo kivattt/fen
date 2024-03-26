276e3fc7a660ea2d9703502b5f2ed1d1b1de66b0
kivattt <kivattt@gmail.com>
2024-03-27 00:32:30 +0100

    Oops, forgot to add that file

---
+++ fileproperties.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"sort"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+
+type FileProperties struct {
+	*tview.Box
+	table map[string]string
+	tableKeysOrdered []string
+	visible bool
+}
+
+func NewFileProperties() *FileProperties {
+	return &FileProperties{
+		Box: tview.NewBox().SetBorder(true).SetTitle("File properties"),
+		visible: false,
+	}
+}
+
+func (fileProperties *FileProperties) SetTable(table map[string]string) {
+	fileProperties.table = table
+
+	fileProperties.tableKeysOrdered = []string{}
+	for k, _ := range fileProperties.table {
+		fileProperties.tableKeysOrdered = append(fileProperties.tableKeysOrdered, k)
+	}
+
+	sort.Strings(fileProperties.tableKeysOrdered)
+}
+
+func (fileProperties *FileProperties) Draw(screen tcell.Screen) {
+	if !fileProperties.visible {
+		return
+	}
+
+	fileProperties.Box.DrawForSubclass(screen, fileProperties)
+
+	x, y, w, _ := fileProperties.GetInnerRect()
+
+	i := 0
+	for _, key := range fileProperties.tableKeysOrdered {
+		tview.Print(screen, key + " " + fileProperties.table[key], x, y + i, w, tview.AlignLeft, tcell.NewRGBColor(0, 255, 255))
+		i++
+	}
+}
