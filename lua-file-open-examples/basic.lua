cf154f4fba55508b6b46a655350a1c39529f079d
kivattt <kivattt@gmail.com>
2024-07-01 12:06:50 +0200

    Add some safe-guarding to lua-file-open-examples/basic.lua

--- lua-file-open-examples/basic.lua
+++ lua-file-open-examples/basic.lua
@@ -5,6 +5,26 @@ if fen.RuntimeOS == "windows" then
 	editor = "notepad"
 end
 
+if editor == nil or editor == "" then
+	print("Could not find text editor, quitting file open script")
+	os.exit(1)
+end
+
+local function onlyHasSpaceCharacters(s)
+	for i = 1, #s do
+		if s:sub(i,i) ~= " " then
+			return false
+		end
+	end
+
+	return true
+end
+
+if onlyHasSpaceCharacters(editor) then
+	print("Environment variable EDITOR only has spaces in it, quitting file open script")
+	os.exit(2)
+end
+
 for i = 1, #fen.SelectedFiles do
 	os.execute(editor.." "..fen.SelectedFiles[i])
 end
