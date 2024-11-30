5102acefdb93e574b9f43bab5330dad7ccce1d56
kivattt <kivattt@gmail.com>
2024-11-30 06:57:24 +0100

    Put git_status at the top of the options menu, bump version

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 	"github.com/rivo/tview"
 )
 
-const version = "v1.7.18"
+const version = "v1.7.19"
 
 func main() {
 	//	f, _ := os.Create("profile.prof")
@@ -1235,6 +1235,7 @@ func main() {
 			}
 
 			optionsAtTheTop := []string{
+				"git_status",
 				"sort_by",
 				"sort_reverse",
 				"ui_borders",
